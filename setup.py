66cb6ff8945ea412c83cbb7cf95f8f41907e2e51
Javier Buzzi <buzzi.javier@gmail.com>
2024-01-18 13:27:56 +0100

    Updates app version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class DjangoTest(TestCommand):
 
 setup(
     name='django_squash',
-    version='0.0.7',
+    version='0.0.8',
     description="A migration squasher that doesn't care how Humpty Dumpty was put together.",
     long_description=README,
     classifiers=[
