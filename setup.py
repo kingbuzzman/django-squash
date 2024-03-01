ffd315130b285ae129f09b773250c952df5758bb
Javier Buzzi <buzzi.javier@gmail.com>
2024-03-01 06:42:52 -0500

    Attempt to not have duplicate logic

--- setup.py
+++ setup.py
@@ -9,58 +9,56 @@ here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as fp:
     README = fp.read()
 
+DJANGO_VERSIONS = ["3.2", "4.1", "4.2", "5.0"]
+PYTHON_VERSIONS = ["3.8", "3.9", "3.11", "3.12"]
+MIN_DJANGO_VERSION = min(DJANGO_VERSIONS)
+MIN_PYTHON_VERSION = min(PYTHON_VERSIONS)
 
-setup(
-    name="django_squash",
-    version="0.0.10",
-    description="A migration squasher that doesn't care how Humpty Dumpty was put together.",
-    long_description=README,
-    classifiers=[
-        # See https://pypi.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.1",
-        "Framework :: Django :: 4.2",
-        "Framework :: Django :: 5.0",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-    ],
-    keywords="django migration squashing squash",
-    author="Javier Buzzi",
-    author_email="buzzi.javier@gmail.com",
-    url="https://github.com/kingbuzzman/django-squash",
-    license="MIT",
-    packages=find_packages(exclude=["tests*", "docs*"]),
-    platforms=["any"],
-    zip_safe=True,
-    python_requires=">=3.8",
-    install_requires=[
-        "django>=3.2",
-    ],
-    tests_require=[],
-    extras_require={
-        "test": [
-            "black",
-            "flake8",
-            "ipdb",
-            "isort",
-            "libcst",
-            "pytest-cov",
-            "pytest-django",
-            "restructuredtext-lint",
+if __name__ == "__main__":
+    setup(
+        name="django_squash",
+        version="0.0.10",
+        description="A migration squasher that doesn't care how Humpty Dumpty was put together.",
+        long_description=README,
+        classifiers=[
+            # See https://pypi.org/pypi?%3Aaction=list_classifiers
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Framework :: Django",
+        ] + [f"Framework :: Django :: {v}" for v in DJANGO_VERSIONS] + [
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+        ] + [f"Programming Language :: Python :: {v}" for v in PYTHON_VERSIONS] + [
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: MIT License",
         ],
-    },
-)
+        keywords="django migration squashing squash",
+        author="Javier Buzzi",
+        author_email="buzzi.javier@gmail.com",
+        url="https://github.com/kingbuzzman/django-squash",
+        license="MIT",
+        packages=find_packages(exclude=["tests*", "docs*"]),
+        platforms=["any"],
+        zip_safe=True,
+        python_requires=f">={MIN_PYTHON_VERSION}",
+        install_requires=[
+            "django>={MIN_DJANGO_VERSION}",
+        ],
+        tests_require=[],
+        extras_require={
+            "test": [
+                "black",
+                "flake8",
+                "ipdb",
+                "isort",
+                "libcst",
+                "pytest-cov",
+                "pytest-django",
+                "restructuredtext-lint",
+            ],
+        },
+    )
