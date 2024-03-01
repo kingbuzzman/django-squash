199868ca270f6a7cd3c6f897026ce9c27a2e8527
kingbuzzman <buzzi.javier@gmail.com>
2024-03-01 17:07:06 -0500

    Clean up

--- django_squash/management/commands/circularmigrations.py
+++ django_squash/management/commands/circularmigrations.py
@@ -1,18 +1,11 @@
-import itertools
-import os
-
-from django.conf import settings
 from django.apps import apps
-from django.core.management.base import BaseCommand, CommandError, no_translations
-from django.db.migrations.loader import MigrationLoader
+from django.conf import settings
+from django.core.management.base import BaseCommand, no_translations
 from django.db.migrations.state import ProjectState
 
-from django_squash import settings as app_settings
-from django_squash.db.migrations import serializer
 from django_squash.db.migrations.autodetector import SquashMigrationAutodetector
 from django_squash.db.migrations.loader import SquashMigrationLoader
 from django_squash.db.migrations.questioner import NonInteractiveMigrationQuestioner
-from django_squash.db.migrations.writer import MigrationWriter
 
 
 class Command(BaseCommand):
@@ -27,7 +20,9 @@ class Command(BaseCommand):
         dependency_list = settings.INSTALLED_APPS
         apps.all_models
 
-        questioner = NonInteractiveMigrationQuestioner(specified_apps=None, dry_run=False)
+        questioner = NonInteractiveMigrationQuestioner(
+            specified_apps=None, dry_run=False
+        )
         squash_loader = SquashMigrationLoader(None, ignore_no_migrations=True)
         autodetector = SquashMigrationAutodetector(
             squash_loader.project_state(),
@@ -35,7 +30,12 @@ class Command(BaseCommand):
             questioner,
         )
 
-        changes = autodetector.changes(squash_loader.graph, trim_to_apps=None, convert_apps=None, migration_name=None)
+        changes = autodetector.changes(
+            squash_loader.graph,
+            trim_to_apps=None,
+            convert_apps=None,
+            migration_name=None,
+        )
         for app_label, migrations in changes.items():
             app_label = apps.get_app_config(app_label).name
             try:
@@ -48,26 +48,52 @@ class Command(BaseCommand):
                 for depends_on_app_label, _ in migration.dependencies:
                     if depends_on_app_label == "__setting__":
                         continue
-                    depends_on_app_label = apps.get_app_config(depends_on_app_label).name
+                    depends_on_app_label = apps.get_app_config(
+                        depends_on_app_label
+                    ).name
                     depends_on_app_ranking = dependency_list.index(depends_on_app_label)
                     if depends_on_app_ranking > app_ranking:
-                        print(f"* {app_label} ({app_ranking}) > {depends_on_app_label} ({depends_on_app_ranking})")
-                        bad_dependencies.append(depends_on_app_label.split('.')[-1])
+                        print(
+                            f"* {app_label} ({app_ranking}) > {depends_on_app_label} ({depends_on_app_ranking})"
+                        )
+                        bad_dependencies.append(depends_on_app_label.split(".")[-1])
 
                 if not bad_dependencies:
                     continue
 
                 references_found = []
                 for operation in migration.operations:
-                    if hasattr(operation, "field") and hasattr(operation.field, "related_model"):
-                        if operation.field.related_model._meta.app_label in bad_dependencies:
-                            references_found.append((operation.model_name, operation.name, operation.field.related_model))
+                    if hasattr(operation, "field") and hasattr(
+                        operation.field, "related_model"
+                    ):
+                        if (
+                            operation.field.related_model._meta.app_label
+                            in bad_dependencies
+                        ):
+                            references_found.append(
+                                (
+                                    operation.model_name,
+                                    operation.name,
+                                    operation.field.related_model,
+                                )
+                            )
 
                     if hasattr(operation, "fields"):
                         for name, field in operation.fields:
-                            if hasattr(field, "related_model") and field.related_model and not isinstance(field.related_model, str):
-                                if field.related_model._meta.app_label in bad_dependencies:
-                                    references_found.append((operation.name, name, field.related_model))
+                            if (
+                                hasattr(field, "related_model")
+                                and field.related_model
+                                and not isinstance(field.related_model, str)
+                            ):
+                                if (
+                                    field.related_model._meta.app_label
+                                    in bad_dependencies
+                                ):
+                                    references_found.append(
+                                        (operation.name, name, field.related_model)
+                                    )
 
                 for model_name, field_name, model in references_found:
-                    print(f"  references {model._meta.app_label}.{model._meta.object_name} via {model_name}.{field_name}")
+                    print(
+                        f"  references {model._meta.app_label}.{model._meta.object_name} via {model_name}.{field_name}"
+                    )
