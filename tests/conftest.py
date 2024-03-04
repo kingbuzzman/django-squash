a5283c15d28d1faa23fa4765fe46512995dd85a6
Javier Buzzi <buzzi.javier@gmail.com>
2024-03-04 05:57:40 -0500

    Robust tests that can accept multiple apps/models (#58)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,24 @@ import io
 import os
 import shutil
 import tempfile
+from collections import defaultdict
+from contextlib import ExitStack
 from importlib import import_module
-from unittest import mock
 
 import pytest
-from django.apps import apps
 from django.core.management import call_command
+from django.db.models.options import Options
 from django.test.utils import extend_sys_path
 from django.utils.module_loading import module_dir
 
 
 @pytest.fixture
-def migration_app_dir(request, settings):
+def migration_app_dir(request, isolated_apps, settings):
     yield from _migration_app_dir("temporary_migration_module", request, settings)
 
 
 @pytest.fixture
-def migration_app2_dir(request, settings):
+def migration_app2_dir(request, isolated_apps, settings):
     yield from _migration_app_dir("temporary_migration_module2", request, settings)
 
 
@@ -39,50 +40,66 @@ def _migration_app_dir(marker_name, request, settings):
 
     app_label = mark.kwargs["app_label"]
     module = mark.kwargs.get("module")
-    join = mark.kwargs.get("join") or False
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        target_dir = tempfile.mkdtemp(dir=temp_dir)
-        with open(os.path.join(target_dir, "__init__.py"), "w"):
-            pass
-        target_migrations_dir = os.path.join(target_dir, "migrations")
-
-        if module is None:
-            module = apps.get_app_config(app_label).name + ".migrations"
-
-        try:
-            source_migrations_dir = module_dir(import_module(module))
-        except (ImportError, ValueError):
-            pass
-        else:
-            shutil.copytree(source_migrations_dir, target_migrations_dir)
-
-        with extend_sys_path(temp_dir):
-            new_module = os.path.basename(target_dir) + ".migrations"
-            modules = {app_label: new_module}
-            if join:
-                modules.update(settings.MIGRATION_MODULES)
-            settings.MIGRATION_MODULES = modules
-            yield target_migrations_dir
+    source_module_path = module_dir(import_module(module))
+    target_module = import_module(settings.MIGRATION_MODULES[app_label])
+    target_module_path = module_dir(target_module)
+    shutil.rmtree(target_module_path)
+    shutil.copytree(source_module_path, target_module_path)
+    yield target_module_path
 
 
 @pytest.fixture(autouse=True)
-def _clean_model(monkeypatch):
+def isolated_apps(settings, monkeypatch):
     """
     Django registers models in the apps cache, this is a helper to remove them, otherwise django throws warnings
     that this model already exists.
     """
-    mock_register_model = mock.Mock(wraps=apps.register_model)
-    monkeypatch.setattr(apps, "register_model", mock_register_model)
+    with ExitStack() as stack:
+        original_apps = Options.default_apps
+        original_all_models = original_apps.all_models
+        original_app_configs = original_apps.app_configs
+        new_all_models = defaultdict(dict)
+        new_app_configs = {}
 
-    yield
+        monkeypatch.setattr("django.apps.apps.all_models", new_all_models)
+        monkeypatch.setattr("django.apps.apps.app_configs", new_app_configs)
+        monkeypatch.setattr("django.apps.apps.stored_app_configs", [])
+        monkeypatch.setattr("django.apps.apps.apps_ready", False)
+        monkeypatch.setattr("django.apps.apps.models_ready", False)
+        monkeypatch.setattr("django.apps.apps.ready", False)
+        monkeypatch.setattr("django.apps.apps.loading", False)
+        monkeypatch.setattr("django.apps.apps._pending_operations", defaultdict(list))
+        installed_app = settings.INSTALLED_APPS.copy()
+        _installed_app = installed_app.copy()
+        _installed_app.remove("django.contrib.auth")
+        _installed_app.remove("django.contrib.contenttypes")
+        original_apps.populate(_installed_app)
 
-    for call in mock_register_model.call_args_list:
-        app_label, model = call.args
-        model_name = model._meta.model_name
-        app_models = apps.all_models[app_label]
-        app_models.pop(model_name)
-        apps.clear_cache()
+        for app_label in {"auth", "contenttypes"}:
+            new_all_models[app_label] = original_all_models[app_label]
+            new_app_configs[app_label] = original_app_configs[app_label]
+
+        temp_dir = tempfile.TemporaryDirectory()
+        stack.enter_context(temp_dir)
+        stack.enter_context(extend_sys_path(temp_dir.name))
+        with open(os.path.join(temp_dir.name, "__init__.py"), "w"):
+            pass
+
+        for app_label in installed_app:
+            target_dir = tempfile.mkdtemp(prefix=f"{app_label}_", dir=temp_dir.name)
+            with open(os.path.join(target_dir, "__init__.py"), "w"):
+                pass
+            migration_path = os.path.join(target_dir, "migrations")
+            os.mkdir(migration_path)
+            with open(os.path.join(migration_path, "__init__.py"), "w"):
+                pass
+            module_name = f"{os.path.basename(target_dir)}.migrations"
+
+            settings.MIGRATION_MODULES[app_label] = module_name
+            stack.enter_context(extend_sys_path(target_dir))
+
+        yield original_apps
 
 
 @pytest.fixture
