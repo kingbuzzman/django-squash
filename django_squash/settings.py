0a7249aa312f70351fb6b55cadb7ecac984de02e
Javier Buzzi <buzzi.javier@gmail.com>
2025-07-30 11:15:51 +0200

    Lint

--- django_squash/settings.py
+++ django_squash/settings.py
@@ -3,9 +3,7 @@ from __future__ import annotations
 from django.conf import settings as global_settings
 from django.utils.functional import lazy
 
-DJANGO_SQUASH_IGNORE_APPS = lazy(
-    lambda: getattr(global_settings, "DJANGO_SQUASH_IGNORE_APPS", None) or [], list
-)()
+DJANGO_SQUASH_IGNORE_APPS = lazy(lambda: getattr(global_settings, "DJANGO_SQUASH_IGNORE_APPS", None) or [], list)()
 DJANGO_SQUASH_MIGRATION_NAME = lazy(
     lambda: getattr(global_settings, "DJANGO_SQUASH_MIGRATION_NAME", None) or "squashed", str
 )()
