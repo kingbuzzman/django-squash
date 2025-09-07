f9d82ac894131c2f1810fd6d4edb15263616863c
Javier Buzzi <buzzi.javier@gmail.com>
2025-09-07 03:09:08 +0200

    Standardize on BigAutoField causing issues with the new main branch (#98)

--- tests/settings.py
+++ tests/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
